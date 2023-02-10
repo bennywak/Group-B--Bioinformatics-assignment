f8e3cf922a6b231e040ade187d5cdc3827cd748f
Benaiah Wakuma <wakumabenaiah@gmail.com>
2023-02-10 03:01:54 +0100

    script added

---
+++ script_Benaiah.R
@@ -0,0 +1,7 @@
+name = readline('name;Benaiah Wakuma
+                email;wakumabenaiah@gmail.com
+                Matric number; UG17/MDHA/1004
+                Programming language used; R
+                Bioinformatic sub-speciality; Genomics
+                Slack username;@bennywak')
+
